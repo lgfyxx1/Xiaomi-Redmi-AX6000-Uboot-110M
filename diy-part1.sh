7fc7f2713f31084f2f51f362f5129deb7d0a7102
lgfyxx1 <68235325+lgfyxx1@users.noreply.github.com>
2025-04-20 09:32:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,6 @@
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git-full passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git-full passwallpackages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
-echo 'src-git-full openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+# echo 'src-git-full passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git-full passwallpackages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
+# echo 'src-git-full openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
